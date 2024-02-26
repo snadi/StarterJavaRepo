d842a1c32f03e01d05bcc7a47edd16264cca2ed4
Akhat <akhatsuleimenov@gmail.com>
2024-02-26 14:29:46 +0400

    check build

--- src/test/java/nyuad/cs3216/CalculatorTest.java
+++ src/test/java/nyuad/cs3216/CalculatorTest.java
@@ -21,7 +21,7 @@ public class CalculatorTest {
     public void divide_WholeDivisible() {
         int a = 12;
         int b = 4;
-        double expected = 3;
+        double expected = 3; // dummy test
         assertEquals(expected, calculator.divide(a, b));
     }
 
