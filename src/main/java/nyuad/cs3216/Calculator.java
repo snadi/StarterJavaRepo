07fc6e6437dceb9e9a86edf4546e33e432176e40
sagynbek001 <st4121@nyu.edu>
2024-02-26 14:46:18 +0400

    added some error

--- src/main/java/nyuad/cs3216/Calculator.java
+++ src/main/java/nyuad/cs3216/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 
    public double divide(int a, int b) {
 
-      double result = (double) a / (double) b;
+      double result = (double) a / (double) b + 1;
       currentValue = result;
       return result;
    }
